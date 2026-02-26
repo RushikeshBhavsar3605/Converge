744d58f4cf6e9b45b46a9445d1c2f4e718a1c0d1
RushikeshBhavsar3605 <rvbhavsar36@gmail.com>
2026-02-26 11:06:55 +0530

    feat: health endpoint

---
+++ app/api/health/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic"; // ensures fresh response (no caching)
+
+export async function GET() {
+  return NextResponse.json({
+    status: "OK",
+    message: "Your API is running",
+    timestamp: new Date().toISOString(),
+  });
+}
